7d6b5c8573ba848ddad085c0a96346642b520db2
Noelia Barranco Godoy <56674140+NotNoe@users.noreply.github.com>
2024-10-12 19:07:15 +0200

    Preprocess data script

--- Codigo/preprocess_data.py
+++ Codigo/preprocess_data.py
@@ -6,28 +6,78 @@ import os
 #load anotations and raw data
 Y = pd.read_csv('./ptbxl/ptbxl_database.csv', index_col='ecg_id')
 Y.scp_codes = Y.scp_codes.apply(lambda x: ast.literal_eval(x))
-Y = pd.DataFrame(Y, columns=['scp_codes', 'strat_fold', 'filename_hr'])
+Y = Y[['scp_codes', 'filename_hr', 'strat_fold']]
 Y = Y.rename(columns={'filename_hr':'filename'})
 validation_fold = 9
 test_fold = 10
 Y_train = Y[(Y.strat_fold != validation_fold) & (Y.strat_fold != test_fold)].drop(columns=['strat_fold'])
 Y_validation = Y[Y.strat_fold == validation_fold].drop(columns=['strat_fold'])
 Y_test = Y[Y.strat_fold == test_fold].drop(columns=['strat_fold'])
+scp_statements = pd.read_csv('./ptbxl/scp_statements.csv', index_col=0)[['diagnostic_class']]
+super_classes = set(scp_statements['diagnostic_class'].unique())
+super_classes = {cls for cls in super_classes if pd.notna(cls)}
+super_classes.add('OTHER')
 
-with open("./ptbxldata/train_REC", 'w+') as f:
+def fill_columns(row:pd.Series) -> pd.Series:
+    if isinstance(row['scp_codes'], str):
+    # Reemplazar comillas simples por dobles y convertir a diccionario
+        cadena_json = row['scp_codes'].replace("'", '"')
+        try:
+            dict_probs = ast.literal_eval(cadena_json)
+        except (ValueError, SyntaxError) as e:
+            print(f"Error al convertir la fila {index}: {e}")
+            return
+    elif isinstance(row['scp_codes'], dict):
+        dict_probs = row['scp_codes']
+    else:
+        print(f"Tipo inesperado en la fila {index}: {type(row['scp_codes'])}")
+        raise NotImplementedError("Tipo no reconocido")
+    #dict_probs ahora es lo que queremos
+
+    for super_class in super_classes:
+        n = 0
+        acum = 0       
+        for diagnosis, prob in dict_probs.items():
+            try:
+                superclass = scp_statements['diagnostic_class'].loc[diagnosis]
+            except KeyError:
+                superclass = 'OTHER'
+            #Si su superclase coincide
+            if superclass != super_class:
+                continue
+            n += 1
+            acum += prob
+        if n == 0:
+            row[super_class] = 0.0
+        else:
+            row[super_class] = acum/n
+    return row
+
+def generate_diagnostic_probs(x:pd.DataFrame) -> pd.DataFrame:
+    for col in super_classes:
+        x[col] = None
+    x = x.apply(fill_columns, axis=1)
+    return x.drop(columns=['scp_codes'])
+            
+Y_train = generate_diagnostic_probs(Y_train)
+Y_validation = generate_diagnostic_probs(Y_validation)
+Y_test = generate_diagnostic_probs(Y_test)
+
+
+with open("./data/train_REC", 'w+') as f:
     for index, row in Y_train.iterrows():
         f.write(row['filename'] + "\n")
-    Y_train.to_csv("./ptbxldata/train_db.csv")
+    Y_train.drop(columns=['filename']).to_csv("./data/train_db.csv")
 
-with open("./ptbxldata/validation_REC", 'w+') as f:
+with open("./data/validation_REC", 'w+') as f:
     for index, row in Y_validation.iterrows():
         f.write(row['filename'] + "\n")
-    Y_validation.to_csv("./ptbxldata/validation_db.csv")
+    Y_validation.drop(columns=['filename']).to_csv("./data/validation_db.csv")
 
-with open("./ptbxldata/test_REC", 'w+') as f:
+with open("./data/test_REC", 'w+') as f:
     for index, row in Y_test.iterrows():
         f.write(row['filename'] + "\n")
-    Y_test.to_csv("./ptbxldata/test_db.csv")
+    Y_test.drop(columns=['filename']).to_csv("./data/test_db.csv")
 
 os.system("python3 ./sergio/generate_h5.py ./data/test_REC ./data/test.hdf5 --root_dir ./ptbxl")
 os.system("python3 ./sergio/generate_h5.py ./data/train_REC ./data/train.hdf5 --root_dir ./ptbxl")
--- Codigo/test.py
+++ Codigo/test.py
@@ -3,7 +3,7 @@ import numpy as np
 import wfdb
 import ast
 import ecg_plot
-from scipy.signal import ShortTimeFFT
+#from scipy.signal import ShortTimeFFT
 import h5py
 
 
@@ -13,10 +13,5 @@ path = './ptbxl/'
 #Y = pd.read_csv(path+'ptbxl_database.csv', index_col='ecg_id')
 signals = wfdb.rdsamp("./ptbxl/records500/00000/00001_hr")[0].transpose() #Devuelve un array de 5000x12
 ecg_plot.plot(signals, sample_rate=500, show_grid=False)
-with h5py.File("./ptbxl/records500.hdf5", 'r') as f:
-    processed_signals = f['tracings'][0,:,:].transpose()
-ecg_plot.plot(processed_signals, sample_rate=400, show_grid=False)
-with h5py.File("./ptbxl/recordsnotch.hdf5", 'r') as f:
-    processed_signals = f['tracings'][0,:,:].transpose()
-ecg_plot.plot(processed_signals, sample_rate=400, show_grid=False)
-ecg_plot.show()
+with h5py.File("./data/train.hdf5", 'r') as f:
+    print(f['tracings'])
