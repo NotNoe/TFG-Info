600931c962b9efc496b947c0dd20ab7c67f20b9f
Noelia Barranco Godoy <noebg2009@volta1.dacya.ucm.es>
2024-10-26 19:39:49 +0200

    Tengo miedo

--- Codigo/ribeiro/train.py
+++ Codigo/ribeiro/train.py
@@ -1,6 +1,7 @@
 from tensorflow.keras.optimizers import Adam
 from tensorflow.keras.callbacks import (ModelCheckpoint, TensorBoard, ReduceLROnPlateau,
                                         CSVLogger, EarlyStopping)
+import tensorflow as tf
 from model import get_model
 import argparse
 from datasets import ECGSequence
@@ -20,37 +21,43 @@ if __name__ == "__main__":
     parser.add_argument('--dataset_name', type=str, default='tracings',
                         help='name of the hdf5 dataset containing tracings')
     args = parser.parse_args()
-    # Optimization settings
-    loss = 'binary_crossentropy'
-    lr = 0.001
-    batch_size = 64
-    opt = Adam(lr)
-    callbacks = [ReduceLROnPlateau(monitor='val_loss',
-                                   factor=0.1,
-                                   patience=7,
-                                   min_lr=lr / 100),
-                 EarlyStopping(patience=9,  # Patience should be larger than the one in ReduceLROnPlateau
-                               min_delta=0.00001)]
 
-    train_seq, valid_seq = ECGSequence.get_train_and_val(
-        args.path_to_hdf5_train, args.path_to_hdf5_val, args.dataset_name, args.path_to_csv_train, args.path_to_csv_val, batch_size)
+    #Train with multiple GPUs
+    strategy = tf.distribute.MirroredStrategy()
+    print('Number of devices: {}'.format(strategy.num_replicas_in_sync))
 
-    # If you are continuing an interrupted section, uncomment line bellow:
-    #   model = keras.models.load_model(PATH_TO_PREV_MODEL, compile=False)
-    model = get_model(train_seq.n_classes)
-    model.compile(loss=loss, optimizer=opt)
-    # Create log
-    callbacks += [TensorBoard(log_dir='./logs', write_graph=False),
-                  CSVLogger('training.log', append=False)]  # Change append to true if continuing training
-    # Save the BEST and LAST model
-    callbacks += [ModelCheckpoint('./backup_model_last.hdf5'),
-                  ModelCheckpoint('./backup_model_best.hdf5', save_best_only=True)]
-    # Train neural network
-    history = model.fit(train_seq,
-                        epochs=70,
-                        initial_epoch=0,  # If you are continuing a interrupted section change here
-                        callbacks=callbacks,
-                        validation_data=valid_seq,
-                        verbose=1)
-    # Save final result
-    model.save("./final_model.hdf5")
+    with strategy.scope():
+        # Optimization settings
+        loss = 'binary_crossentropy'
+        lr = 0.001
+        batch_size = 64
+        opt = Adam(lr)
+        callbacks = [ReduceLROnPlateau(monitor='val_loss',
+                                    factor=0.1,
+                                    patience=7,
+                                    min_lr=lr / 100),
+                    EarlyStopping(patience=9,  # Patience should be larger than the one in ReduceLROnPlateau
+                                min_delta=0.00001)]
+
+        train_seq, valid_seq = ECGSequence.get_train_and_val(
+            args.path_to_hdf5_train, args.path_to_hdf5_val, args.dataset_name, args.path_to_csv_train, args.path_to_csv_val, batch_size)
+
+        # If you are continuing an interrupted section, uncomment line bellow:
+        #   model = keras.models.load_model(PATH_TO_PREV_MODEL, compile=False)
+        model = get_model(train_seq.n_classes)
+        model.compile(loss=loss, optimizer=opt)
+        # Create log
+        callbacks += [TensorBoard(log_dir='./logs', write_graph=False),
+                    CSVLogger('training.log', append=False)]  # Change append to true if continuing training
+        # Save the BEST and LAST model
+        callbacks += [ModelCheckpoint('./backup_model_last.hdf5'),
+                    ModelCheckpoint('./backup_model_best.hdf5', save_best_only=True)]
+        # Train neural network
+        history = model.fit(train_seq,
+                            epochs=70,
+                            initial_epoch=0,  # If you are continuing a interrupted section change here
+                            callbacks=callbacks,
+                            validation_data=valid_seq,
+                            verbose=1)
+        # Save final result
+        model.save("./final_model.hdf5")
