0a535c53a28cc67599f566f4760743a7935c32a5
Noelia Barranco Godoy <noebg2009@volta1.dacya.ucm.es>
2024-12-18 15:46:48 +0100

    Explicaciones

--- Codigo/QoL/explain.py
+++ Codigo/QoL/explain.py
@@ -13,12 +13,12 @@ logging.getLogger('tensorflow').setLevel(logging.ERROR)
 
 perfect = json.load(open("out.json"))
 ETIQUETAS = ["CD", "HYP", "MI", "NORM", "STTC"]
-#Load the datasets
+# Load the datasets
 test_x = h5py.File('./data/test.hdf5', 'r')["tracings"]
 # Load the labels from the CSV file
 df = pd.read_csv('./data/test_db.csv')
 test_y = df.drop(columns=['ecg_id']).to_numpy()
-#Load model
+# Load model
 model = load_model("./final_models/original_model.hdf5")
 
 strategy = tf.distribute.MirroredStrategy()
@@ -30,23 +30,27 @@ for label in ETIQUETAS:
     for item in array:
         ITEM_IDX = item["linea"] - 2
         ecg_id = item["ecg_id"]
-
-
+        if ecg_id in [157, 180, 448, 598, 618, 514]:
+            continue
 
         with strategy.scope():
-            explainer = TSR(model, test_x.shape[-2], test_x.shape[-1], mode="time", method="IG", device="cuda")
+            explainer = TSR(
+                model, test_x.shape[-2], test_x.shape[-1], mode="time", method="IG", device="cuda")
             item = test_x[ITEM_IDX:ITEM_IDX+1, :, :]
             prediction = model.predict(item)[0]
-            #Imprime los índices de las etiquetas >0.5
+            # Imprime los índices de las etiquetas >0.5
             print(prediction > 0.5)
-            #Explica solo la primera etiqueta "true"
+            # Explica solo la primera etiqueta "true"
             for label_idx in range(prediction.shape[0]):
                 if prediction[label_idx] > 0.5:
                     explanation = explainer.explain(item, label_idx)
                     break
 
-        os.makedirs(f"./out/explanations/original_model/{label}/{ecg_id}", exist_ok=True)
-        #save the explanation ndarray
-        np.save(f"./out/explanations/original_model/{label}/{ecg_id}/explanation.npy", explanation)
+        os.makedirs(
+            f"./out/explanations/original_model/{label}/{ecg_id}", exist_ok=True)
+        # save the explanation ndarray
+        np.save(
+            f"./out/explanations/original_model/{label}/{ecg_id}/explanation.npy", explanation)
         with plt.ioff():
-            explainer.plot(item, explanation, figsize=(15,60), save=f"./out/explanations/original_model/{label}/{ecg_id}/explanation.png")
+            explainer.plot(item, explanation, figsize=(
+                15, 60), save=f"./out/explanations/original_model/{label}/{ecg_id}/explanation.png")
