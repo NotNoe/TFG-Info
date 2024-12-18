c7a814fd6bfe256ddf64bce9dbff60f95d9feb61
Noelia Barranco Godoy <56674140+NotNoe@users.noreply.github.com>
2024-12-18 21:04:30 +0100

    bug fix

--- Codigo/ribeiro/datasets.py
+++ Codigo/ribeiro/datasets.py
@@ -16,38 +16,16 @@ class ECGSequence(Sequence):
 
     def __init__(self, path_to_hdf5, hdf5_dset, path_to_csv=None, batch_size=8,
                  start_idx=0, end_idx=None, labels = None, dataset_labels = 12, fill_with_zeroes=False):
+        self.labes = labels
+        self.dataset_labels = dataset_labels
+        self.fill_with_zeroes = fill_with_zeroes
         if path_to_csv is None:
             self.y = None
         else:
             self.y = pd.read_csv(path_to_csv, index_col=0).values
         # Get tracings
         self.f = h5py.File(path_to_hdf5, "r")
-        data = self.f[hdf5_dset]
-        if labels is None:
-            self.x = self.f[hdf5_dset]
-        elif fill_with_zeroes:
-            M = data.shape[1]
-            if M % dataset_labels != 0:
-                raise ValueError("The number of labels is not a multiple of dataset_labels")
-            block_size = M//dataset_labels
-            self.x = self.f[hdf5_dset]
-            labels_to_zero = [i for i in range(1, dataset_labels+1) if i not in labels] #Los que no estan en labels
-            for i in labels_to_zero:
-                start = (i-1)*block_size
-                end = i*block_size
-                self.x[:,start:end,:] = 0
-        else: #En este caso solamente sacamos las que estan en labels
-            M = data.shape[1]
-            #Si M no es divisible por dataset_labels, hay un error
-            if M % dataset_labels != 0:
-                raise ValueError("The number of labels is not a multiple of dataset_labels")
-            block_size = M//dataset_labels
-            submatrices = []
-            for i in labels:
-                start = (i-1)*block_size
-                end = i*block_size
-                submatrices.append(data[:,start:end,:])
-            self.x = np.concatenate(submatrices, axis=1)
+        self.x = self.f[hdf5_dset]
         self.batch_size = batch_size
         if end_idx is None:
             end_idx = len(self.x)
@@ -61,10 +39,32 @@ class ECGSequence(Sequence):
     def __getitem__(self, idx):
         start = self.start_idx + idx * self.batch_size
         end = min(start + self.batch_size, self.end_idx)
+        batch_x = np.array(self.x[start:end, :, :]).astype(np.float32)
+        if self.labels is not None and self.fill_with_zeroes:
+            M = batch_x.shape[1]
+            if M % self.dataset_labels != 0:
+                raise ValueError("The number of labels is not a multiple of dataset_labels")
+            block_size = M//self.dataset_labels
+            labels_to_zero = [i for i in range(1, self.dataset_labels+1) if i not in self.labels]
+            for i in labels_to_zero:
+                start = (i-1)*block_size
+                end = i*block_size
+                batch_x[:,start:end,:] = 0
+        elif self.labels is not None and not self.fill_with_zeroes:
+            M = batch_x.shape[1]
+            if M % self.dataset_labels != 0:
+                raise ValueError("The number of labels is not a multiple of dataset_labels")
+            block_size = M//self.dataset_labels
+            selected_blocs = []
+            for label_id in self.labels:
+                start = (label_id-1)*block_size
+                end = label_id*block_size
+                selected_blocs.append(batch_x[:,start:end,:])
+            batch_x = np.concatenate(selected_blocs, axis=1)
         if self.y is None:
-            return np.array(self.x[start:end, :, :]).astype(np.float32)
+            return batch_x
         else:
-            return np.array(self.x[start:end, :, :]).astype(np.float32), np.array(self.y[start:end])
+            return batch_x, np.array(self.y[start:end])
 
     def __len__(self):
         return math.ceil((self.end_idx - self.start_idx) / self.batch_size)
